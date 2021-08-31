68d1c56d2526c5c8fc3f3f587c8f2350f303603b
raihannissa123 <88261183+raihannissa123@users.noreply.github.com>
2021-08-31 22:54:46 +0700

    Add Transaksi navbar

--- src/assets/components/Navbar.js
+++ src/assets/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import * as FaIcons from "react-icons/fa";
-// import * as AiIcons from "react-icons/ai";
+import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { SidebarData } from './SidebarData';
 import './Navbar.css';
