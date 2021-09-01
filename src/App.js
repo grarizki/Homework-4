50bbea48cfd0f574fb60acc35e8b40500f9fef69
grarizki <28901232+grarizki@users.noreply.github.com>
2021-09-01 20:04:11 +0700

    naming route path, comment transactions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navbar from './assets/components/Navbar';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/status/Home';
-// import Transactions from './pages/status/Transactions';
 import Logout from './pages/status/Logout';
 import Transaksi from './pages/status/Transaksi';
 import './pages/status/Transaksi.css';
-
+// import Transactions from './pages/status/Transactions';
 // import Reports from './pages/Reports';
 // import  from './pages/Products';
 
@@ -18,7 +17,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/reports' exact component={Transaksi} />
+          <Route path='/transaksi' exact component={Transaksi} />
           <Route path='/signout' exact component={Logout} />
         </Switch>
       </Router>
