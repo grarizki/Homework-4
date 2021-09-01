8840e0ee0bb5e443c0c1f627afc1dfa131c7c5c2
grarizki <28901232+grarizki@users.noreply.github.com>
2021-09-01 23:31:56 +0700

    fix posistion and navbarcomponent

--- src/pages/status/Logout.js
+++ src/pages/status/Logout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavbarComponent from "../../assets/components/navbar/NavbarComponent"
+import '../status/logout.css'
 
 function Logout() {
   return (
-    <div className='logout'>
+    <div>
       <NavbarComponent />
+    <div className='logout'>
       <h1>Logout</h1>
+    </div>
     </div>
   );
 }
