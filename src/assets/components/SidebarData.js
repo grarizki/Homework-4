6f5f09c239098e561f8ca870369c59292954b5c4
grarizki <28901232+grarizki@users.noreply.github.com>
2021-09-01 20:04:33 +0700

    add new page and sidebar menu

--- src/assets/components/SidebarData.js
+++ src/assets/components/SidebarData.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import * as IoIcons from "react-icons/io";
+import * as BsIcons from "react-icons/bs";
 
 export const SidebarData = [
     {
@@ -12,8 +13,14 @@ export const SidebarData = [
     },
     {
         title: 'Transactions',
-        path: '/reports',
+        path: '/transaksi',
         icon: <IoIcons.IoIosPaper />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Agents',
+        path: '/agent',
+        icon: <BsIcons.BsFillPeopleFill />,
         cName: 'nav-text'
     },
     {
